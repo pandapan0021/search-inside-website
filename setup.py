9e0d639e5f12460812fa147763b47293ffb79661
pandapan <pandapan@localhost.localdomain>
2016-11-01 02:49:35 +0800

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+#-*- coding: utf8 -*-
+
+from setuptools import setup
+
+
+setup(
+        name='searcher',
+        version='0.1',
+        install_requires=[
+                          'jieba==0.38'
+                          'redis==2.10.5'
+                          ]
+)
